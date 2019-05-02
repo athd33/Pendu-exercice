77df2cf9f2d12bf93ce2ac11bf2154c4e76da6cc
athd33 <antoinecours@gmx.fr>
2019-05-02 13:34:08 +0200

    Correctifs return/global ok. Reste commentaires à faire

--- fonctions.py
+++ fonctions.py
@@ -1,14 +1,13 @@
 #coding:utf-8
 
 import time
-from random import choice
 from donnees import *
+from random import choice
+import pickle
 
-
-def myprint(word):
+def myprint(word):          #creation d'une fonction print() incluant un time.sleep
     time.sleep(1)
     print(word)
-
 
 
 def get_user_name():
@@ -17,7 +16,7 @@ def get_user_name():
 
 def get_user_letter():
     letter = input("Veuillez choisir une lettre : ")
-    print("Verification de la lettre")
+    myprint("Verification de la lettre")
     try:
         letter = int(letter)
         myprint("Attention, vous devez entrer une lettre")
@@ -29,10 +28,12 @@ def get_user_letter():
             get_user_letter()
         return letter
 
+
 def get_random_word():
     with open("donnees.py", "r") as fichier:
         word = choice(liste_mots)
     return word
+
 
 def display_secret_word(word, good_word):
     secret_word = ""
@@ -42,4 +43,30 @@ def display_secret_word(word, good_word):
         else:
             secret_word += "*"
     return secret_word
+
+
+def init_score():
+    print("Initialisation du score en cours")
+    try:
+        with open("data", "rb") as fichier:
+            record = pickle.Unpickler(fichier)
+            score = record.load()
+    except:
+        score = {}
+        print("Aucun fichier data trouvé")
+        print("Data vient d'être créé")
+    return score
+
+def save_score(scores):
+    print("Enregistrement en cours")
+    with open("data", "wb") as fichier:
+        record = pickle.Pickler(fichier)
+        record.dump(scores)
+
+
+
+
+
+
+
 
--- pendu.py
+++ pendu.py
@@ -1,31 +1,62 @@
 #coding:utf-8
 
-
-
 from fonctions import *
 
 game = True
 good_letter = []
 tentatives = 8
 word = ""
+compte  = 1
+
 
 print("Recherche de mot en cours...")
 random_word = get_random_word()
 print(f"Random word : {random_word}")
 
+
+score = init_score()
+
 user_name = get_user_name()
 print(f"Bienvenu {user_name}, nous allons commencer...")
+print(f"Affichage de score {score}")
+if user_name in score:
+    print("Encore vous?")
+
 
 while game:
-
     user_letter = get_user_letter()
+    tentatives -= 1
+
+    if tentatives < 1:
+        print("Fin de partie, vous avez perdu!!!")
+        exit()
+
+    print(f"Tentatives : {tentatives}")
     myprint(f"print de user_letter : {user_letter}")
 
     for letter in random_word:
         if letter == user_letter:
             print("Bravo")
+            print(f"Compte : {compte}")
+            compte += 1
+            tentatives += 1
             good_letter += user_letter
 
+    if compte == len(word) +1:
+        print(f"BRAVO!!! , vous avez marqué {compte} points!")
+        score[user_name] = compte
+        print(f"print de score {score}")
+        save_score(score)
+        print(f"Fin de partie, merci d'avoir joué {user_name}")
+        exit()
+
+
+    print(f"Tentatives restantes {tentatives}")
     word = display_secret_word(random_word, good_letter)
     print(word)
+
+
+
+
+
 
